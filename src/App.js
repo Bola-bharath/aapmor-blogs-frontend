7fa0cd031b10419d5e5bf6586aff5f555412cfe0
Bharath <dbbk2002@gmail.com>
2023-11-02 17:43:04 +0530

    added provider

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login.js";
 import Home from "./components/HomePage/home.js";
 import CreateBlog from "./components/CreateBlog/postblog.js";
+import { Provider } from "react-redux";
+import store from "../src/components/Store/store";
 
 const App = () => {
   return (
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route exact path="/login" element={<Login />} />
-      <Route exact path="/createblog" element={<CreateBlog />} />
-    </Routes>
+    <Provider store={store}>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/createblog" element={<CreateBlog />} />
+      </Routes>
+    </Provider>
   );
 };
 
