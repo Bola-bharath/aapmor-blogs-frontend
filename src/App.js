1662621c726a0a5ec08f05c8c7b217264b1fb624
saipraveen <praveensai18@gmail.com>
2023-11-02 16:55:18 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login.js";
-import Register from "./components/Register/Register.js";
 import Home from "./components/HomePage/home.js";
 // import Blog from "./components/CreateBlog/postblog.js";
 
