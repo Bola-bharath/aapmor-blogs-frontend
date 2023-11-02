b65f2ec5c88b5aa655ebfcc3e1a57e37d9dcbe24
Bharath <dbbk2002@gmail.com>
2023-11-02 12:10:28 +0530

    changed login api

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login.js";
 import Register from "./components/Register/Register.js";
-//import ForgetPassword from "./components/ForgetPassword/ForgetPassword.js";
 import Home from "./components/HomePage/home.js";
-import Blog from "./components/CreateBlog/postblog.js";
+// import Blog from "./components/CreateBlog/postblog.js";
 
 const App = () => {
   return (
@@ -12,8 +11,6 @@ const App = () => {
       <Route exact path="/" element={<Home />} />
       <Route exact path="/login" element={<Login />} />
       <Route exact path="/register" element={<Register />} />
-
-      {/* <Route exact path="/forgetPassword" element={<ForgetPassword />} /> */}
     </Routes>
   );
 };
