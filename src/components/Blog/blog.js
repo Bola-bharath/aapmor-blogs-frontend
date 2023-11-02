46180d21390e36532b48f0b58b624be8710ef968
Bharath <dbbk2002@gmail.com>
2023-11-02 17:31:45 +0530

    bharat

--- src/components/Blog/blog.js
+++ src/components/Blog/blog.js
@@ -1,15 +1,20 @@
 import {
   Avatar,
   Box,
+  Button,
   Card,
+  CardActions,
   CardContent,
   CardHeader,
   CardMedia,
+  IconButton,
   Typography,
 } from "@mui/material";
 import React from "react";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 
 const Blog = (blogDetails) => {
+  console.log(blogDetails);
   const {
     category,
     comments,
@@ -21,39 +26,70 @@ const Blog = (blogDetails) => {
     username,
     userrole,
     _id,
-  } = blogDetails;
+  } = blogDetails.blogDetails;
   return (
-    <Box>
+    <Box bgcolor={"background.default"} color={"text.primary"}>
       <Card sx={{ maxWidth: 345 }}>
         <CardHeader
           avatar={
             <Avatar sx={{ bgcolor: "red" }} aria-label="profile-image">
-              {username}
+              {username.slice(0, 1)}
             </Avatar>
           }
           title={username}
           subheader={date}
         />
-        <CardMedia
+        <Box
+          sx={{
+            backgroundImage: `url(
+              "https://images.freeimages.com/variants/2kqnXDhNwJR2Us1oQgfU7E9N/f4a36f6589a0e50e702740b15352bc00e4bfaf6f58bd4db850e167794d05993d"
+            )`,
+            backgroundSize: "cover",
+            height: 200,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-end",
+          }}
+        >
+          {/* <Box
+            sx={{
+              background:
+                "linearGradient(195deg, #ffffff 32.93%, #ffffff 89.72%)",
+            }}
+          >
+            <Typography>{username}</Typography>
+            <Typography>{userrole}</Typography>
+          </Box> */}
+        </Box>
+        {/* <CardMedia
           sx={{ height: 200, width: 345 }}
           image="https://images.freeimages.com/variants/2kqnXDhNwJR2Us1oQgfU7E9N/f4a36f6589a0e50e702740b15352bc00e4bfaf6f58bd4db850e167794d05993d"
           title="blog image"
-        />
+        /> */}
 
         <CardContent>
           <Typography variant="body2" color={"#D3D3D3"}>
             {category}
           </Typography>
           <Typography variant="h6" fontWeight={700}>
-            T{title}
+            {title}
           </Typography>
           <Typography variant="body1">
-            {description}
-            <a href="#" style={{ textDecoration: "none", color: "#000EE6" }}>
-              ReadMore...
-            </a>
+            {description.slice(0, 100)}...
           </Typography>
         </CardContent>
+        <CardActions
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Button variant="text">Read More...</Button>
+          <IconButton>
+            <BookmarkBorderIcon />
+          </IconButton>
+        </CardActions>
       </Card>
     </Box>
   );
--- src/components/HomePage/Footer.js
+++ src/components/HomePage/Footer.js
@@ -1,19 +1,184 @@
-import { Box } from "@mui/material";
+import { Grid, Paper, Typography } from "@mui/material";
+
 import React from "react";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const Footer = () => {
   return (
-    <Box
-    /* sx={{
-        position: "fixed",
-        width: "100%",
-        top: "80vh",
-        bottom: 0,
-        backgroundColor: "#D2EDFF",
-      }} */
+    <Paper
+      elevation={5}
+      bgcolor={"background.default"}
+      color={"text.primary"}
+      sx={{
+        display: "flex",
+        justifyContent: "space-evenly",
+        paddingTop: "20px",
+        paddingBottom: "10px",
+      }}
     >
-      <h1>Footer</h1>
-    </Box>
+      <Grid item xs={2}>
+        <Typography
+          variant="h4"
+          gutterBottom
+          sx={{
+            fontFamily: "cambria math",
+            color: "#000EE6",
+          }}
+        >
+          AAPMOR
+        </Typography>
+        <Grid item>
+          <TwitterIcon
+            sx={{ color: "#00acee", height: "30px", width: "30px" }}
+          />
+          <FacebookIcon
+            color="primary"
+            sx={{ color: "#1877F2", height: "30px", width: "30px" }}
+          />
+          <InstagramIcon
+            sx={{ color: "#ed2147", height: "30px", width: "30px" }}
+          />
+          <LinkedInIcon
+            sx={{ color: " #0072b1 ", height: "30px", width: "30px" }}
+          />
+        </Grid>
+      </Grid>
+      <Grid item xs={3}>
+        <Typography
+          variant="h6"
+          gutterBottom
+          sx={{ fontFamily: "cambria Math", fontWeight: "bold" }}
+        >
+          Solutions
+        </Typography>
+        <Typography
+          variant="body1"
+          gutterBottom
+          sx={{ fontFamily: "cambria Math" }}
+        >
+          Fill Stack Development
+        </Typography>
+        <Typography
+          variant="body1"
+          gutterBottom
+          sx={{ fontFamily: "cambria Math" }}
+        >
+          SAP
+        </Typography>
+        <Typography
+          variant="body1"
+          gutterBottom
+          sx={{ fontFamily: "cambria Math" }}
+        >
+          HCM
+        </Typography>
+        <Typography
+          variant="body1"
+          gutterBottom
+          sx={{ fontFamily: "cambria Math" }}
+        >
+          Testing Automation
+        </Typography>
+        <Typography
+          variant="body1"
+          gutterBottom
+          sx={{ fontFamily: "cambria Math" }}
+        >
+          Data Analytics
+        </Typography>
+        <Typography
+          variant="body1"
+          gutterBottom
+          sx={{ fontFamily: "cambria Math" }}
+        >
+          Cloud Transformation
+        </Typography>
+        <Typography
+          variant="body1"
+          gutterBottom
+          sx={{ fontFamily: "cambria Math" }}
+        >
+          Rist & Audit Mgmt
+        </Typography>
+        <Typography
+          variant="body1"
+          gutterBottom
+          sx={{ fontFamily: "cambria Math" }}
+        >
+          Intelligent Automation
+        </Typography>
+        <Typography
+          variant="body1"
+          gutterBottom
+          sx={{ fontFamily: "cambria Math" }}
+        >
+          Identity Management
+        </Typography>
+      </Grid>
+      <Grid item xs={3}>
+        <Typography
+          variant="h6"
+          gutterBottom
+          sx={{ fontFamily: "cambria Math", fontWeight: "bold" }}
+        >
+          Quick Links
+        </Typography>
+        <Typography
+          variant="body1"
+          gutterBottom
+          sx={{ fontFamily: "cambria Math" }}
+        >
+          Home
+        </Typography>
+        <Typography
+          variant="body1"
+          gutterBottom
+          sx={{ fontFamily: "cambria Math" }}
+        >
+          About us
+        </Typography>
+        <Typography
+          variant="body1"
+          gutterBottom
+          sx={{ fontFamily: "cambria Math" }}
+        >
+          Contact us
+        </Typography>
+      </Grid>
+      <Grid item xs={3}>
+        <Typography
+          variant="h6"
+          gutterBottom
+          sx={{ fontFamily: "cambria Math", fontWeight: "bold" }}
+        >
+          Connect to us
+        </Typography>
+        <Typography
+          variant="body1"
+          gutterBottom
+          sx={{ fontFamily: "cambria Math" }}
+        >
+          Phone : +19724138201
+        </Typography>
+        <Typography
+          variant="body1"
+          gutterBottom
+          sx={{ fontFamily: "cambria Math" }}
+        >
+          Email : info@aapmor.com
+        </Typography>
+        <Typography
+          variant="body1"
+          gutterBottom
+          sx={{ fontFamily: "cambria Math" }}
+        >
+          www.aapmor.com
+        </Typography>
+      </Grid>
+    </Paper>
   );
 };
 
