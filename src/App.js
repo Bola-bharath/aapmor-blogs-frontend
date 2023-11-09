0123e947e242bf8f2389d4291615a9971b108eef
pranay0880 <pranay0880@gmail.com>
2023-11-09 11:55:02 +0530

    commented theme mode

--- src/App.js
+++ src/App.js
@@ -10,27 +10,27 @@ import { indigo } from "@mui/material/colors";
 import CreateBlog from "./components/CreateBlog/postblog.js";
 
 const App = () => {
-  const themObj = useSelector((state) => state.navbar);
-  const darkTheme = createTheme({
-    palette: {
-      mode: themObj.mode,
-      palette: {
-        primary: indigo,
-        secondary: {
-          main: "#0d47a1",
-        },
-      },
-    },
-  });
+  // const themObj = useSelector((state) => state.navbar);
+  // const darkTheme = createTheme({
+  //   palette: {
+  //     mode: themObj.mode,
+  //     palette: {
+  //       primary: indigo,
+  //       secondary: {
+  //         main: "#0d47a1",
+  //       },
+  //     },
+  //   },
+  // });
   return (
-    <ThemeProvider theme={darkTheme}>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/createblog" element={<CreateBlog />} />
-        <Route exact path="/blogs/:id" element={<BlogView />} />
-      </Routes>
-    </ThemeProvider>
+    // <ThemeProvider theme={darkTheme}>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/login" element={<Login />} />
+      <Route exact path="/createblog" element={<CreateBlog />} />
+      <Route exact path="/blogs/:id" element={<BlogView />} />
+    </Routes>
+    // </ThemeProvider>
   );
 };
 //APP MODIFIED
